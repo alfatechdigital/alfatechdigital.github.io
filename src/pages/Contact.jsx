0f1972e341b2bd1bcdca4ce20f6f5c67994914f6
Rizky Ramadhani <ramadhanriz57@gmail.com>
2025-08-18 09:12:57 +0700

    add maps in contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -127,6 +127,17 @@ const Contact = () => {
                   </div>
                 ))}
               </div>
+
+              <div className="p-4 bg-white rounded-lg overflow-hidden shadow relative">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.7805286946973!2d112.23263752417115!3d-7.706684726336069!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e785d5706dea647%3A0x253307097bd8d028!2sJatirejo%2C%20Jombok%2C%20Ngoro%2C%20Jombang%20Regency%2C%20East%20Java!5e0!3m2!1sen!2sid!4v1755481751310!5m2!1sen!2sid"
+                  width="100%"
+                  height="450"
+                  allowFullScreen
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </div>
             </div>
           </div>
         </div>
